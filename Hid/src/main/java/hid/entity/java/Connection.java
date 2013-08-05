8961698ed2a682b11fb99a64ce89d4a876ff8835
student <student@uac1>
2013-08-05 12:06:37 +0300

    Add java-entity

---
+++ Hid/src/main/java/hid/entity/java/Connection.java
@@ -0,0 +1,104 @@
+package hid.entity.java;
+
+import java.util.Date;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Table(name = "CONNECTION")
+class Connection {
+
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "ID_CONNECTION", nullable = false)
+	private long id;
+	
+	@NotEmpty
+	@Column(name = "ID_DEVICE", nullable = false)
+	private long deviceId;
+	
+	@NotEmpty
+	@Column(name = "DATE", nullable = false)
+	private Date date;
+	
+	@NotEmpty
+	@Column(name = "DATA", nullable = false)
+	private byte[] data;
+	
+	@NotEmpty
+	@Column(name = "COUNTRY", length = 32, nullable = false)
+	private String country;
+	
+	@NotEmpty
+	@Column(name = "CITY", length = 32, nullable = false)
+	private String city;
+	
+	@NotEmpty
+	@Column(name = "CONNECTION_TIME", nullable = false)
+	private int connectionTime;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(long deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getConnectionTime() {
+		return connectionTime;
+	}
+
+	public void setConnectionTime(int connectionTime) {
+		this.connectionTime = connectionTime;
+	}
+}
---
+++ Hid/src/main/java/hid/entity/java/RegDevice.java
@@ -0,0 +1,65 @@
+package hid.entity.java;
+
+import java.util.Date;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "REG_DEVICES")
+class RegDevice {
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "ID_REG_DEVICE", nullable = false)
+	private long id;
+	
+	@NotEmpty
+	@Column(name = "ID_DEVICE", nullable = false)
+	private long deviceId;
+	
+	@NotEmpty
+	@Column(name = "REG_DATE", nullable = false)
+	private Date registrationDate;
+	
+	@NotEmpty
+	@Column(name = "END_LICENSE", nullable = false)
+	private Date endLicenseDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(long deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Date getEndLicenseDate() {
+		return endLicenseDate;
+	}
+
+	public void setEndLicenseDate(Date endLicenseDate) {
+		this.endLicenseDate = endLicenseDate;
+	}
+}
